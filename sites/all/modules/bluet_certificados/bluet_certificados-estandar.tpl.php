af71cb782b74746d6e0b8005348214fb8e4b4589
oscarfabian91 <oscfor@eltiempo.com>
2017-01-30 17:19:10 -0500

    Implementación modulo certificados

---
+++ sites/all/modules/bluet_certificados/bluet_certificados-estandar.tpl.php
@@ -0,0 +1,39 @@
+<fieldset id="certificado-estandar-contenedor" class='marco-certificado contenedor-certificado'>
+  <div id="certificado-estandar-contenedor-superior">
+    <div id="certificado-estandar-logo" class="alinear-centro">
+      <img src="<?php echo $certificado_url_logo_cliente ?>" />	
+    </div>
+  </div>
+  <div id="certificado-estandar-contenedor-central" class="alinear-centro">
+    <br>
+    <p><?php echo $linea1; ?></p>
+    <br>
+    <p class="negrita letra-tamano-14pt"><?php echo $linea2; ?></p>
+    <p class="negrita"><?php echo $linea3; ?></p>
+    <p><?php echo $linea4; ?></p>
+    <br>
+    <p><?php echo $linea5; ?></p>
+    <p class="negrita letra-tamano-18pt mayuscula"><?php echo $linea6; ?></p>
+    <p class="negrita"><?php echo $linea7; ?></p>
+  	<br>
+  	<p><?php echo $linea8; ?></p>
+  	<p class="negrita letra-tamano-18pt"><?php echo $linea9; ?></p>
+  	<p class="negrita"><?php echo $linea10; ?></p>
+  	<br>
+  </div>
+  <div id="certificado-estandar-contenedor-inferior">
+    <p class="alinear-centro"><?php echo $linea11; ?></p>
+    <br>
+    <div class="float-left ancho600 alinear-derecha">
+      <p class="negrita"><?php echo $linea12; ?></p>
+      <p><?php echo $linea13; ?></p>
+      <p><?php echo $linea14; ?></p>
+      <p><?php echo $linea15; ?></p>
+      <p><?php echo $linea16; ?></p>
+      <p><?php echo $linea17; ?></p>
+    </div>
+    <div class="float-left ancho280 padding-izq8">
+    	<img src="<?php echo $certificado_entrenador_url_firma ?>">	
+    </div>	
+  </div>
+</fieldset>
